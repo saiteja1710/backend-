fedef3f1dc49a24a91956ec0dada1e04ab63e0b3
yesh9836 <saiyeshwanthrajanala2312@gmail.com>
2025-05-07 20:20:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin:  "https://681b6f74a04a553cdf5b9871--resonant-toffee-47f057.netlify.app/",
+    origin:  "https://resonant-toffee-47f057.netlify.app/",
     methods: ["GET", "POST", "OPTIONS"]
     
   }
 });
-
-// Serve static files (frontend)
-app.use(express.static('frontend'));
 
 io.on('connection', socket => {
   console.log(`User connected: ${socket.id}`);
